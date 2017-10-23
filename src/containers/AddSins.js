808850d42ad6943de1dd8b31046156a479d699a4
fadetoblack1 <fadetoblack1@yandex.ru>
2017-10-24 02:07:37 +0300

    started add sin component

--- src/containers/AddSins.js
+++ src/containers/AddSins.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, TextInput } from 'react-native';
 import { Icon, Text } from 'react-native-elements';
 
 class AddSins extends React.Component {
+    constructor(props) {
+        super(props);
+        this.form = {};
+        this.state = {circle: 1};
+    }
+
     static navigationOptions = {
         tabBarIcon: ({ tintColor }) => (
             <Icon
@@ -16,6 +22,19 @@ class AddSins extends React.Component {
         return (
             <ScrollView style={{backgroundColor: '#222', flex: 1, paddingTop: 25, paddingLeft: 5, paddingRight: 5}} >
                 <Text h4 style={{ color: '#fff' }}> Що Ви накоїли? </Text>
+                    <TextInput 
+                        style={{height: 50, color: '#fff', paddingLeft: 10, paddingRight: 10, borderColor: '#fff'}}
+                        placeholder='Наприклад "збрехав"'
+                        returnKeyType='next'
+                        ref={(input) => this.form.input = input}
+                        />
+                    <TextInput 
+                        style={{height: 90, color: '#fff', paddingLeft: 10, paddingRight: 10, borderColor: '#fff'}}
+                        multiline={true}
+                        numberOfLines={4}
+                        placeholder='Коментар'
+                        returnKeyType='next'
+                        ref={(textarea) => this.form.textarea = textarea}/>
             </ScrollView>
         );    
     }
